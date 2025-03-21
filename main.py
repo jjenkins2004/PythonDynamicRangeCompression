7aac61e0df6362683fc82b0d0cebfb11c314b05d
Joshua Jenkins <2004joshuaj@gmail.com>
2025-03-21 11:43:13 -0700

    moving files

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from compressor import compress_audio
+from dynamic_range_compression import compress_audio
 import os
+import time
 
 # Define file paths.
 input_folder = "in"
 output_folder = "out"
-input_file = os.path.join(input_folder, "test.mp3")
-output_file = os.path.join(output_folder, "test_compressed.mp3")
+input_file = os.path.join(input_folder, "test.m4a")
+output_file = os.path.join(output_folder, "test_compressed.m4a")
 
 # Ensure the output folder exists.
 os.makedirs(output_folder, exist_ok=True)
@@ -19,7 +20,14 @@ lookAhead = 10          # Lookahead time in ms
 attack = 5              # Attack time in ms
 release = 50            # Release time in ms
 
+# Start timer.
+start_time = time.time()
+
 # Call compress_audio to process the file.
 compress_audio(input_file, output_file, threshold, makeupGain, kneeWidth, compressionRatio, lookAhead, attack, release)
 
+# Stop timer.
+elapsed_time = time.time() - start_time
+
 print("Compression complete. File saved to:", output_file)
+print("Time taken: {:.2f} seconds".format(elapsed_time))
